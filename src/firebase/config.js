81451fa64b57d5215f33ea976a4ac9342f1b298a
Janeth1513 <94811417+Janeth1513@users.noreply.github.com>
2022-06-27 17:14:42 -0600

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbPI7KP2mKvPHCoCEcXUGzGDLuWL19f9U",
+  authDomain: "actividad-15-d439e.firebaseapp.com",
+  databaseURL: "https://actividad-15-d439e-default-rtdb.firebaseio.com",
+  projectId: "actividad-15-d439e",
+  storageBucket: "actividad-15-d439e.appspot.com",
+  messagingSenderId: "936783883259",
+  appId: "1:936783883259:web:f48d05d86edcd5f5a3395e"
+};
+
+const db = initializeApp(firebaseConfig);
+
+export default getDatabase(db);
